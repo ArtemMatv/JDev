dc9e190a61013f1afa6ae811881599eb513e60cb
ArtemMatv <kvazar2569@gmail.com>
2020-09-28 22:14:52 +0300

    Some fixes for queue and stack

--- simpleCollectionsTask/src/com/company/Main.java
+++ simpleCollectionsTask/src/com/company/Main.java
@@ -2,17 +2,7 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        ArrayList list = new ArrayList(8);
-        list.add(5);
-        list.add(4);
-        list.add(3);
-        list.add(2);
-        list.add(1);
+    public static void main(String[] args) {
 
-        list.remove(2);
-        for (int i = 0; i < list.size(); i++){
-            System.out.println(list.get(i));
-        }
     }
 }
--- simpleCollectionsTask/src/com/company/Stack.java
+++ simpleCollectionsTask/src/com/company/Stack.java
@@ -18,15 +18,30 @@ public class Stack {
         return _length;
     }
 
-    private void push(int item){
+    public void push(int item){
         Node newNode = new Node(item);
         newNode.next = _head;
         _head = newNode;
+        _length++;
     }
 
-    private int pop(){
+    public int pop() throws Exception {
+        if (_head == null)
+            throw new Exception("There are no elements");
         int result = _head.getValue();
         _head = _head.next;
+        _length--;
         return result;
+    }
+
+    public boolean contains(int item){
+        Node elem = _head;
+        while(elem != null){
+            if (elem.getValue() == item)
+                return true;
+            elem = elem.next;
+        }
+
+        return false;
     }
 }
