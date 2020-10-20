5ac2ed82bc6861a00093a132e2cdfb0816e8f347
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2020-10-20 13:05:42 +0300

    String calculator is done

--- AllTheWorks/src/stringTasks/Main.java
+++ AllTheWorks/src/stringTasks/Main.java
@@ -12,7 +12,13 @@ public class Main {
         String input = new Scanner(System.in).nextLine();
 
         int result = 0;
+<<<<<<< Updated upstream
         for (int i = 0; i < input.length(); i++){
+=======
+        int i = 0;
+
+        while ( i < input.length()){
+>>>>>>> Stashed changes
             char act;
             if (!(input.charAt(i) >= 48 && input.charAt(i) <= 57)) {
                 act = input.charAt(i);
@@ -22,9 +28,18 @@ public class Main {
             {
                 act = '+';
             }
+<<<<<<< Updated upstream
             int start = i;
             while (i < input.length()-1 && (input.charAt(i) >= 48 && input.charAt(i) <= 57))
                 i++;
+=======
+
+            int start = i;
+
+            while (i < input.length() && (input.charAt(i) >= 48 && input.charAt(i) <= 57))
+                i++;
+
+>>>>>>> Stashed changes
             int number = Integer.parseInt(input.substring(start, i));
 
             switch (act){
@@ -35,7 +50,12 @@ public class Main {
                 default: break;
             }
         }
+<<<<<<< Updated upstream
 
         System.out.println(result);
+=======
+        System.out.println(result);
+
+>>>>>>> Stashed changes
     }
 }
