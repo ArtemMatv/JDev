d9f5f0662dac6310122e6f1cc4d25aea89cbd335
ArtemMatv <46032437+ArtemMatv@users.noreply.github.com>
2020-10-20 13:26:30 +0300

    String calculator is done

--- AllTheWorks/src/stringTasks/Main.java
+++ AllTheWorks/src/stringTasks/Main.java
@@ -8,34 +8,47 @@ public class Main {
         countString();
     }
 
-    static private void countString(){
+    static private void countString() {
         String input = new Scanner(System.in).nextLine();
 
         int result = 0;
-        for (int i = 0; i < input.length(); i++){
+
+        int i = 0;
+
+        while (i < input.length()) {
+
             char act;
             if (!(input.charAt(i) >= 48 && input.charAt(i) <= 57)) {
                 act = input.charAt(i);
                 i++;
-            }
-            else
-            {
+            } else {
                 act = '+';
             }
+
             int start = i;
-            while (i < input.length()-1 && (input.charAt(i) >= 48 && input.charAt(i) <= 57))
+            while (i < input.length() && (input.charAt(i) >= 48 && input.charAt(i) <= 57))
                 i++;
+
             int number = Integer.parseInt(input.substring(start, i));
 
-            switch (act){
-                case '+': result += number; break;
-                case '-': result -= number; break;
-                case '/': result = result / number; break;
-                case '*': result *= number; break;
-                default: break;
+            switch (act) {
+                case '+':
+                    result += number;
+                    break;
+                case '-':
+                    result -= number;
+                    break;
+                case '/':
+                    result = result / number;
+                    break;
+                case '*':
+                    result *= number;
+                    break;
+                default:
+                    break;
             }
         }
-
         System.out.println(result);
+
     }
 }
